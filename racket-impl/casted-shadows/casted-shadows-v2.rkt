7ce1ffe9438237b0d6e9a572d3c79513c12dd7a3
radian628 <70352880+radian628@users.noreply.github.com>
2022-08-16 21:43:15 -0700

    better optimized shadowcaster

--- racket-impl/casted-shadows/casted-shadows-v2.rkt
+++ racket-impl/casted-shadows/casted-shadows-v2.rkt
@@ -25,27 +25,27 @@
 
 (define (shadowcast-pos-dimension dim)
     `(= ,(symbol-append 'shadowcast-positions dim)
-        (for 
+        ;(for 
             (+
                 (*
-                    (get shadowcast-barycentric-coord-1 combined-index)
-                    (get ,(symbol-append dim '-vpos-r2) (get vertex-indices (+ receiver-index2 1)))
+                    shadowcast-barycentric-coord-1
+                    (get ,(symbol-append dim '-vpos-r2) (get vertex-indices receiver-data))
                 )
                 (*
-                    (get shadowcast-barycentric-coord-2 combined-index)
-                    (get ,(symbol-append dim '-vpos-r2)  (get vertex-indices (+ receiver-index2 2)))
+                    shadowcast-barycentric-coord-2
+                    (get ,(symbol-append dim '-vpos-r2)  (get vertex-indices (+ 1 receiver-data)))
                 )
                 (*
                     (- 1 
-                        (get shadowcast-barycentric-coord-1 combined-index)
-                        (get shadowcast-barycentric-coord-2 combined-index))
-                    (get ,(symbol-append dim '-vpos-r2)  (get vertex-indices (+ receiver-index2 0)))
+                        shadowcast-barycentric-coord-1 
+                        shadowcast-barycentric-coord-2 )
+                    (get ,(symbol-append dim '-vpos-r2)  (get vertex-indices (+ 2 receiver-data)))
                 )
             )
-            (tri-index2 (list 0 1 2))
-            (caster-index2 (list 1 4 ... (- (length vertex-indices) 1)))
-            (receiver-index2 (list 1 4 ... (- (length vertex-indices) 1)))
-        )
+        ;     (tri-index2 (list 0 1 2))
+        ;     (caster-index2 (list 1 4 ... (- (length vertex-indices) 1)))
+        ;     (receiver-index2 (list 1 4 ... (- (length vertex-indices) 1)))
+        ; )
     )
 )
 
@@ -134,14 +134,14 @@
         (for 
             (piecewise
                 ; eliminate cases where a triangle shadows itself
-                ((= caster-index receiver-index) (point -1 -1))
+                ((= caster-index receiver-index) (point -11111 -1))
 
                 ; eliminate cases where the receiver isn't facing the light
                 ((> (+ 
                     (* (get x-normals (/ (+ 2 receiver-index) 3)) light-dir-x)
                     (* (get y-normals (/ (+ 2 receiver-index) 3)) light-dir-y)
                     (* (get z-normals (/ (+ 2 receiver-index) 3)) light-dir-z)
-                ) 0) (point -1 -1))
+                ) 0) (point -11111 -1))
 
                 ; eliminate cases where the caster is facing the light
                 ; will not work if the scene has single-sided geometry
@@ -149,65 +149,49 @@
                     (* (get x-normals (/ (+ 2 caster-index) 3)) light-dir-x)
                     (* (get y-normals (/ (+ 2 caster-index) 3)) light-dir-y)
                     (* (get z-normals (/ (+ 2 caster-index) 3)) light-dir-z)
-                ) 0) (point -1 -1))
+                ) 0) (point -11111 -1))
 
-                ; eliminate cases where the receiver is in front of the caster
-                ((>
+                ; ; eliminate cases where the receiver is in front of the caster
+                ; ((>
 
-                    (min
-                        (+
-                            (* (get x-vpos (+ receiver-index 0)) light-dir-x)
-                            (* (get y-vpos (+ receiver-index 0)) light-dir-y)
-                            (* (get z-vpos (+ receiver-index 0)) light-dir-z)
-                        )
-                        (+
-                            (* (get x-vpos (+ receiver-index 1)) light-dir-x)
-                            (* (get y-vpos (+ receiver-index 1)) light-dir-y)
-                            (* (get z-vpos (+ receiver-index 1)) light-dir-z)
-                        )
-                        (+
-                            (* (get x-vpos (+ receiver-index 2)) light-dir-x)
-                            (* (get y-vpos (+ receiver-index 2)) light-dir-y)
-                            (* (get z-vpos (+ receiver-index 2)) light-dir-z)
-                        )
-                    )
+                ;     (min
+                ;         (+
+                ;             (* (get x-vpos (+ receiver-index 0)) light-dir-x)
+                ;             (* (get y-vpos (+ receiver-index 0)) light-dir-y)
+                ;             (* (get z-vpos (+ receiver-index 0)) light-dir-z)
+                ;         )
+                ;         (+
+                ;             (* (get x-vpos (+ receiver-index 1)) light-dir-x)
+                ;             (* (get y-vpos (+ receiver-index 1)) light-dir-y)
+                ;             (* (get z-vpos (+ receiver-index 1)) light-dir-z)
+                ;         )
+                ;         (+
+                ;             (* (get x-vpos (+ receiver-index 2)) light-dir-x)
+                ;             (* (get y-vpos (+ receiver-index 2)) light-dir-y)
+                ;             (* (get z-vpos (+ receiver-index 2)) light-dir-z)
+                ;         )
+                ;     )
 
-                    (max
-                        (+
-                            (* (get x-vpos (+ caster-index 0)) light-dir-x)
-                            (* (get y-vpos (+ caster-index 0)) light-dir-y)
-                            (* (get z-vpos (+ caster-index 0)) light-dir-z)
-                        )
-                        (+
-                            (* (get x-vpos (+ caster-index 1)) light-dir-x)
-                            (* (get y-vpos (+ caster-index 1)) light-dir-y)
-                            (* (get z-vpos (+ caster-index 1)) light-dir-z)
-                        )
-                        (+
-                            (* (get x-vpos (+ caster-index 2)) light-dir-x)
-                            (* (get y-vpos (+ caster-index 2)) light-dir-y)
-                            (* (get z-vpos (+ caster-index 2)) light-dir-z)
-                        )
-                    )
+                ;     (max
+                ;         (+
+                ;             (* (get x-vpos (+ caster-index 0)) light-dir-x)
+                ;             (* (get y-vpos (+ caster-index 0)) light-dir-y)
+                ;             (* (get z-vpos (+ caster-index 0)) light-dir-z)
+                ;         )
+                ;         (+
+                ;             (* (get x-vpos (+ caster-index 1)) light-dir-x)
+                ;             (* (get y-vpos (+ caster-index 1)) light-dir-y)
+                ;             (* (get z-vpos (+ caster-index 1)) light-dir-z)
+                ;         )
+                ;         (+
+                ;             (* (get x-vpos (+ caster-index 2)) light-dir-x)
+                ;             (* (get y-vpos (+ caster-index 2)) light-dir-y)
+                ;             (* (get z-vpos (+ caster-index 2)) light-dir-z)
+                ;         )
+                ;     )
 
-                ) (point -1 -1))
+                ; ) (point -1 -1))
 
-                ; ray-triangle intersection for shadowcasting
-                ; ((muller-trumbore-full-data-with-invalid-barycentrics
-                ;     (+ (* light-dir-x 0.005) (get x-vpos (get vertex-indices (+ tri-index caster-index))))
-                ;     (+ (* light-dir-y 0.005) (get y-vpos (get vertex-indices (+ tri-index caster-index))))
-                ;     (+ (* light-dir-z 0.005) (get z-vpos (get vertex-indices (+ tri-index caster-index))))
-                ;     light-dir-x light-dir-y light-dir-z
-                ;     (get x-vpos (get vertex-indices receiver-index))
-                ;     (get y-vpos (get vertex-indices receiver-index))
-                ;     (get z-vpos (get vertex-indices receiver-index))
-                ;     (get x-vpos (get vertex-indices (+ receiver-index 1)))
-                ;     (get y-vpos (get vertex-indices (+ receiver-index 1)))
-                ;     (get z-vpos (get vertex-indices (+ receiver-index 1)))
-                ;     (get x-vpos (get vertex-indices (+ receiver-index 2)))
-                ;     (get y-vpos (get vertex-indices (+ receiver-index 2)))
-                ;     (get z-vpos (get vertex-indices (+ receiver-index 2)))
-                ; ))
                 ((get ray-triangle-intersections (+
                     (get vertex-indices (+ caster-index tri-index))
                     (* (/ (- receiver-index 1) 3) (length x-vpos))
@@ -218,9 +202,16 @@
             (receiver-index (list 1 4 ... (- (length vertex-indices) 1)))
         )
     )
-    (= shadowcast-barycentric-coord-1 (/ (- (mod (.y shadowcast-data) 67108864) 33554432) 4096))
-    (= shadowcast-barycentric-coord-2 (/ (- (floor (/ (.y shadowcast-data) 67108864)) 33554432) 4096))
+    (= filtered-shadowcast-data (get shadowcast-data shadowcast-broadphase-filter))
+
+    (= shadowcast-barycentric-coord-1 (/ (- (mod (.y filtered-shadowcast-data) 67108864) 33554432) 4096))
+    (= shadowcast-barycentric-coord-2 (/ (- (floor (/ (.y filtered-shadowcast-data) 67108864)) 33554432) 4096))
     (= shadowcast-barycentric-coord-3 (- 1 shadowcast-barycentric-coord-1 shadowcast-barycentric-coord-2))
+
+    (= shadowcast-broadphase-filter (get 
+        (list 1 ... (length shadowcast-data)) 
+        (> (.x shadowcast-data) -11111)    
+    ))
 
     (= combined-index (
         + 1
@@ -239,28 +230,28 @@
     ,(shadowcast-pos-dimension 'z)
 
     (= caster-data
-        (get vertex-indices (for 
+        (get vertex-indices (get (for 
             (+ caster-index tri-index)
             (tri-index (list 0 1 2))
             (caster-index (list 1 4 ... (- (length vertex-indices) 1)))
             (receiver-index (list 1 4 ... (- (length vertex-indices) 1)))
-        ))
+        ) shadowcast-broadphase-filter))
     )
     (= caster-indices
-        (for 
+        (get (for 
             caster-index
             (tri-index (list 0 1 2))
             (caster-index (list 1 4 ... (- (length vertex-indices) 1)))
             (receiver-index (list 1 4 ... (- (length vertex-indices) 1)))
-        )
+        ) shadowcast-broadphase-filter)
     )
     (= receiver-data
-        (for 
+        (get (for 
             receiver-index
             (tri-index (list 0 1 2))
             (caster-index (list 1 4 ... (- (length vertex-indices) 1)))
             (receiver-index (list 1 4 ... (- (length vertex-indices) 1)))
-        )
+        ) shadowcast-broadphase-filter)
     )
 
     ; (= shadowcast-positions-x (+ (get x-vpos caster-data) (* light-dir-x (.x shadowcast-data))))
@@ -295,9 +286,9 @@
                 ; (1)
 
                 ; version with false positives
-                ((< 0 (.x (get shadowcast-data (list 1 4 ... (- (length shadowcast-positions) 1))))) 1)
-                ((< 0 (.x (get shadowcast-data (list 2 5 ... (- (length shadowcast-positions) 1))))) 1) 
-                ((< 0 (.x (get shadowcast-data (list 3 6 ... (- (length shadowcast-positions) 1))))) 1)
+                ((< 0 (.x (get filtered-shadowcast-data (list 1 4 ... (- (length shadowcast-positions) 1))))) 1)
+                ((< 0 (.x (get filtered-shadowcast-data (list 2 5 ... (- (length shadowcast-positions) 1))))) 1) 
+                ((< 0 (.x (get filtered-shadowcast-data (list 3 6 ... (- (length shadowcast-positions) 1))))) 1)
                 (0)
             ))
         )
@@ -307,23 +298,23 @@
 
     (= shadowcast-polygon-x (
         +
-        (* (.x shadowcast-polygon) (get x-vpos-r2 (get vertex-indices (+ (get receiver-data receiver-data-index) 1))))
-        (* (.y shadowcast-polygon) (get x-vpos-r2 (get vertex-indices (+ (get receiver-data receiver-data-index) 2))))
-        (* (- 1 (.x shadowcast-polygon) (.y shadowcast-polygon)) (get x-vpos-r2 (get vertex-indices (+ (get receiver-data receiver-data-index) 0))))
+        (* (.x shadowcast-polygon) (get x-vpos-r2 (get vertex-indices (+ (get filtered-receiver-data receiver-data-index) 1))))
+        (* (.y shadowcast-polygon) (get x-vpos-r2 (get vertex-indices (+ (get filtered-receiver-data receiver-data-index) 2))))
+        (* (- 1 (.x shadowcast-polygon) (.y shadowcast-polygon)) (get x-vpos-r2 (get vertex-indices (+ (get filtered-receiver-data receiver-data-index) 0))))
     ))
 
     (= shadowcast-polygon-y (
         +
-        (* (.x shadowcast-polygon) (get y-vpos-r2 (get vertex-indices (+ (get receiver-data receiver-data-index) 1))))
-        (* (.y shadowcast-polygon) (get y-vpos-r2 (get vertex-indices (+ (get receiver-data receiver-data-index) 2))))
-        (* (- 1 (.x shadowcast-polygon) (.y shadowcast-polygon)) (get y-vpos-r2 (get vertex-indices (+ (get receiver-data receiver-data-index) 0))))
+        (* (.x shadowcast-polygon) (get y-vpos-r2 (get vertex-indices (+ (get filtered-receiver-data receiver-data-index) 1))))
+        (* (.y shadowcast-polygon) (get y-vpos-r2 (get vertex-indices (+ (get filtered-receiver-data receiver-data-index) 2))))
+        (* (- 1 (.x shadowcast-polygon) (.y shadowcast-polygon)) (get y-vpos-r2 (get vertex-indices (+ (get filtered-receiver-data receiver-data-index) 0))))
     ))
 
     (= shadowcast-polygon-z (
         +
-        (* (.x shadowcast-polygon) (get z-vpos-r2 (get vertex-indices (+ (get receiver-data receiver-data-index) 1))))
-        (* (.y shadowcast-polygon) (get z-vpos-r2 (get vertex-indices (+ (get receiver-data receiver-data-index) 2))))
-        (* (- 1 (.x shadowcast-polygon) (.y shadowcast-polygon)) (get z-vpos-r2 (get vertex-indices (+ (get receiver-data receiver-data-index) 0))))
+        (* (.x shadowcast-polygon) (get z-vpos-r2 (get vertex-indices (+ (get filtered-receiver-data receiver-data-index) 1))))
+        (* (.y shadowcast-polygon) (get z-vpos-r2 (get vertex-indices (+ (get filtered-receiver-data receiver-data-index) 2))))
+        (* (- 1 (.x shadowcast-polygon) (.y shadowcast-polygon)) (get z-vpos-r2 (get vertex-indices (+ (get filtered-receiver-data receiver-data-index) 0))))
     ))
 
     (fn (transform-shadowcast-polygon shadowcast-polygon receiver-data-index)
@@ -339,14 +330,19 @@
         ;)
     )
 
-    (= shadow-caster-normal-index (/ (+ 2 (get caster-indices caster-data-index)) 3))
+
+    (= filtered-receiver-data (get receiver-data shadowcast-coord-filter))
+    (= filtered-caster-data (get caster-data shadowcast-coord-filter))
+    (= filtered-caster-indices (get caster-indices shadowcast-coord-filter))
+
+    (= shadow-caster-normal-index (/ (+ 2 (get filtered-caster-indices caster-data-index)) 3))
     (= shadow-caster-normal-x (get x-normals-r2 shadow-caster-normal-index))
     (= shadow-caster-normal-y (get y-normals-r2 shadow-caster-normal-index))
     (= shadow-caster-normal-z (get z-normals-r2 shadow-caster-normal-index))
 
-    (= shadow-caster-vpos-x (get x-vpos-r2 (get vertex-indices (get caster-indices caster-data-index))))
-    (= shadow-caster-vpos-y (get y-vpos-r2 (get vertex-indices (get caster-indices caster-data-index))))
-    (= shadow-caster-vpos-z (get z-vpos-r2 (get vertex-indices (get caster-indices caster-data-index))))
+    (= shadow-caster-vpos-x (get x-vpos-r2 (get vertex-indices (get filtered-caster-indices caster-data-index))))
+    (= shadow-caster-vpos-y (get y-vpos-r2 (get vertex-indices (get filtered-caster-indices caster-data-index))))
+    (= shadow-caster-vpos-z (get z-vpos-r2 (get vertex-indices (get filtered-caster-indices caster-data-index))))
 
     (fn (eliminate-invalid-shadows shadowcast-polygon receiver-data-index caster-data-index)
         (piecewise
@@ -388,12 +384,13 @@
         3)
     )
 
+
     (= polygon-depths (join
         nonshadow-polygon-depths
-        (- (get (get nonshadow-polygon-depths 
+        (- (get nonshadow-polygon-depths 
             (/ 
-                (+ 2 (get receiver-data (list 1 4 ... (- (length receiver-data) 1))))
-            3)) shadowcast-filter) 0.1)
+                (+ 2 (get filtered-receiver-data (list 1 4 ... (- (length filtered-receiver-data) 1))))
+            3)) 0.1)
     ))
 
     (= brightnesses (min 0.1 (+ (* x-normals light-dir-x) (* y-normals light-dir-y) (* z-normals light-dir-z))))
@@ -411,20 +408,31 @@
             )) 0 0)
 
             (rgb (* 256 0.5 -1 0
-                (get brightnesses (/ (+ 2 (get receiver-data shadowcast-filter)) 3))
+                (get brightnesses (/ (+ 2 filtered-receiver-data) 3))
             ) 0 0)
 
         ) (* -1 polygon-depths)))
     )
 
+    (= shadowcast-coord-filter (for
+        (+ -2 (* (get shadowcast-filter shadowcast-filter-index) 3) coord-index)
+        (coord-index (list 0 1 2))
+        (shadowcast-filter-index (list 1 ... (length shadowcast-filter)))
+    ))
 
+    (= filtered-shadowcast-barycentric-coord-1 
+        (get shadowcast-barycentric-coord-1 shadowcast-coord-filter))
+    (= filtered-shadowcast-barycentric-coord-2 
+        (get shadowcast-barycentric-coord-2 shadowcast-coord-filter))
+    (= filtered-shadowcast-barycentric-coord-3 
+        (get shadowcast-barycentric-coord-3 shadowcast-coord-filter))
     ;(display
         ; (= shadowcast-polygons (get (polygon
         ;     (get shadowcast-positions (list 1 4 ... (length shadowcast-positions)))
         ;     (get shadowcast-positions (list 2 5 ... (length shadowcast-positions)))
         ;     (get shadowcast-positions (list 3 6 ... (length shadowcast-positions)))
         ; ) shadowcast-filter))
-        (= shadowcast-polygons (get 
+        (= shadowcast-polygons ;(get 
         
         (for  ;; fix this!
             (polygon 
@@ -438,24 +446,25 @@
                         ; (get proj-pos (get vertex-indices (+ 2 (get receiver-data shadowcast-index))))
                         (point 0 0) (point 1 0) (point 0 1)
                         (point
-                            (get shadowcast-barycentric-coord-1 shadowcast-index)
-                            (get shadowcast-barycentric-coord-2 shadowcast-index)
+                            (get filtered-shadowcast-barycentric-coord-1 shadowcast-index)
+                            (get filtered-shadowcast-barycentric-coord-2 shadowcast-index)
                         )
                         (point
-                            (get shadowcast-barycentric-coord-1 (+ shadowcast-index 1))
-                            (get shadowcast-barycentric-coord-2 (+ shadowcast-index 1))
+                            (get filtered-shadowcast-barycentric-coord-1 (+ shadowcast-index 1))
+                            (get filtered-shadowcast-barycentric-coord-2 (+ shadowcast-index 1))
                         )
                         (point
-                            (get shadowcast-barycentric-coord-1 (+ shadowcast-index 2))
-                            (get shadowcast-barycentric-coord-2 (+ shadowcast-index 2))
+                            (get filtered-shadowcast-barycentric-coord-1 (+ shadowcast-index 2))
+                            (get filtered-shadowcast-barycentric-coord-2 (+ shadowcast-index 2))
                         )
                     )
                 shadowcast-index shadowcast-index) shadowcast-index)
             )
-            (shadowcast-index (list 1 4 ... (- (length shadowcast-positions) 1)))
+            (shadowcast-index (list 1 4 ... (- (length filtered-shadowcast-barycentric-coord-1) 1)))
         )
         
-        shadowcast-filter))
+        ;shadowcast-filter)
+    )
     ;     (color-latex (rgb 0 0 0))
     ;     (lines #f)
     ;     (fill-opacity 0.5)
